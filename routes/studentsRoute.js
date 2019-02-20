65bb9195abdfbc53490e1efc3a4f9fcc28aa6f9a
bhumip214 <bhumip214@gmail.com>
2019-02-20 15:53:16 -0800

    Completed all CRUD operations for students

--- routes/studentsRoute.js
+++ routes/studentsRoute.js
@@ -39,4 +39,68 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+// Create students request
+router.post("/", async (req, res) => {
+  try {
+    if (req.body.name && req.body.cohort_id) {
+      const [id] = await db("students").insert(req.body);
+
+      const student = await db("students")
+        .where({ id })
+        .first();
+      res.status(201).json(student);
+    } else {
+      res.status(400).json({
+        errorMessage: "Please provide the name and cohort id for the student."
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "There was an error while saving the student to the database"
+    });
+  }
+});
+
+//Update student request
+router.put("/:id", async (req, res) => {
+  try {
+    const count = await db("students")
+      .where({ id: req.params.id })
+      .update(req.body);
+
+    if (count > 0) {
+      const student = await db("students")
+        .where({ id: req.params.id })
+        .first();
+      res.status(200).json(student);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The student with the specified ID does not exist." });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "The student information could not be modified." });
+  }
+});
+
+//delete student request
+router.delete("/:id", async (req, res) => {
+  try {
+    const count = await db("students")
+      .where({ id: req.params.id })
+      .del();
+
+    if (count > 0) {
+      res.status(204).end();
+    } else {
+      res
+        .status(404)
+        .json({ message: "The student with the specified ID does not exist." });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "The student could not be deleted" });
+  }
+});
 module.exports = router;
